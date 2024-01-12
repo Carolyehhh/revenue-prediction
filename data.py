ff2b17d165d170514e30c8697492e8032216cece
Carolyehhh <carolyehhh@gmail.com>
2024-01-12 10:02:36 +0800

    找Logit Model預測率最高、最低的五個產業

--- data.py
+++ data.py
@@ -76,4 +76,4 @@ stock_info = pd.read_sql(stock_qr_sub, conn)
 #================== Overall GDP Data Cleaning ===========================================
 # Import training data
 OverallGeneralEcon = pd.read_csv('C:/Users/user1/Desktop/Cmoney/上市櫃營收預測/10301_11203_GDP.csv')
-print(type(OverallGeneralEcon))
+#print(type(OverallGeneralEcon))
